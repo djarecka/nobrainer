cdeb939e3dfb2acc20fa3c2f416b03e23665e5ff
= <jakubk@mit.edu>
2019-04-24 15:18:23 -0400

    only enable v2 behavior for tf 1.x

--- nobrainer/__init__.py
+++ nobrainer/__init__.py
@@ -2,18 +2,18 @@ from distutils.version import LooseVersion
 
 import tensorflow as tf
 
-# For tensorflow<2.0
-tf.enable_v2_behavior()
+tf_version = LooseVersion(tf.__version__)
 
-if LooseVersion(tf.__version__) < LooseVersion("1.13.1"):
+if tf_version < LooseVersion("1.13.1"):
     raise ValueError(
         "tensorflow>=1.13.1 must be installed but found version {}"
         .format(tf.__version__))
-del LooseVersion, tf
+elif tf_version < LooseVersion("2.0.0-alpha0"):
+    tf.enable_v2_behavior()
 
 from nobrainer._version import get_versions
 __version__ = get_versions()['version']
-del get_versions
+del get_versions, LooseVersion, tf, tf_version
 
 import nobrainer.io
 import nobrainer.layers
